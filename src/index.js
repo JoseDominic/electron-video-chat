ff1869320a264291574ce1b500d3bcc03aa074fc
josedominic <josudominic007@gmail.com>
2020-11-02 15:04:44 +0530

    minor bug fix

--- src/index.js
+++ src/index.js
@@ -5,50 +5,53 @@ const videoGrid = document.getElementById('video-grid')
 
 const socket = io('https://video-sock-server.herokuapp.com/') //arg of io is url of signalling server
 
+//specify second argument object {host:domainname,port:value} to Peer() to use custom peer server
+//default is peerjs open cloud server if no second argument is specified
+const myPeer = new Peer(undefined,{
+  host:'video-peer-server.herokuapp.com',
+  port:443,
+  secure:true,
+  path:'/'
+})
+
+let ROOM_ID
+
 btnGoRoom.addEventListener('click', (event) => {
   if(inputRoomNumber === '') {
     alert('Enter a room number')
   }
   else{
-    const ROOM_ID = inputRoomNumber.value
+    ROOM_ID = inputRoomNumber.value
     console.log(ROOM_ID)
-    //specify second argument object {host:domainname,port:value} to Peer() to use custom peer server
-    //default is peerjs open cloud server if no second argument is specified
-    const myPeer = new Peer(undefined,{
-      host:'video-peer-server.herokuapp.com',
-      port:443,
-      secure:true,
-      path:'/'
-    })
-    const myVideo = document.createElement('video')
-    myVideo.muted = true
-    const peers = {}
-    navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true
-    }).then(stream => {
-      addVideoStream(myVideo, stream)
-      myPeer.on('call', call => {
-        call.answer(stream)
-        const video = document.createElement('video')
-        call.on('stream', userVideoStream => {
-          addVideoStream(video, userVideoStream)
-        })
-      })
-    
-      socket.on('user-connected', userId => {
-        connectToNewUser(userId, stream)
-      })
-    })
-    
-    socket.on('user-disconnected', userId => {
-      if (peers[userId]) peers[userId].close()
-    })
-    
-    myPeer.on('open', id => {
-      socket.emit('join-room', ROOM_ID, id)
-    })
   }
+  const myVideo = document.createElement('video')
+  myVideo.muted = true
+  const peers = {}
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  }).then(stream => {
+    addVideoStream(myVideo, stream)
+    myPeer.on('call', call => {
+      call.answer(stream)
+      const video = document.createElement('video')
+      call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+      })
+    })
+  
+    socket.on('user-connected', userId => {
+      connectToNewUser(userId, stream)
+    })
+  })
+  
+  socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close()
+  })
+  
+  myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+  })
 })
 
 
